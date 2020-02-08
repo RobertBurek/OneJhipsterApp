a796de5f8bad8c21501d06d02ed3f591f9d220de
Robert Burek <robertburek@wp.pl>
2020-02-08 13:18:13 +0100

    pierwsza zmiana widoku

--- src/main/java/pl/robertburek/OneJhipsterApp.java
+++ src/main/java/pl/robertburek/OneJhipsterApp.java
@@ -81,7 +81,7 @@ public class OneJhipsterApp {
             log.warn("The host name could not be determined, using `localhost` as fallback");
         }
         log.info("\n----------------------------------------------------------\n\t" +
-                "Application '{}' is running! Access URLs:\n\t" +
+                "Aplikacja '{}' jest uruchomiona! Dostęp URLs:\n\t" +
                 "Local: \t\t{}://localhost:{}{}\n\t" +
                 "External: \t{}://{}:{}{}\n\t" +
                 "Profile(s): \t{}\n----------------------------------------------------------",
